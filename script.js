175424025010ef33821bef5b110a7e795d28bc21
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2023-04-19 01:28:37 +0500

    All Screen Related Functions Completed

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Variable
+const mainHeading = document.getElementById('main-heading')
+const describingWords = document.getElementById('describing-words')
 const playPauseBtn = document.getElementById('play-pause-btn')
 const videoContainer = document.getElementById('video-container')
 const video = document.getElementById('video')
 const theatreMode = document.getElementById('theatre-mode')
+const fullScreenMode = document.getElementById('fullscreen-mode')
+const miniplayerMode = document.getElementById('miniplayer-mode')
 
 // Event Listeners
 /* 1- For Play/Pause Btn */ 
@@ -18,6 +22,23 @@ video.addEventListener('click' , () => {
 /* 3- For Theatre */
 theatreMode.addEventListener('click' , () => {
     theatre();
+})
+
+/* 4- For FullScreen */
+fullScreenMode.addEventListener('click' , () => {
+    fullScreen();
+})
+
+/* 5- For MiniPlayer */
+miniplayerMode.addEventListener('click' , () => {
+    if(videoContainer.classList.contains('miniplayer')){
+        document.exitPictureInPicture();
+        videoContainer.classList.remove('miniplayer')
+    }
+    else{
+        videoContainer.classList.add('miniplayer')
+        video.requestPictureInPicture();
+    }
 })
 
 // Functions 
@@ -41,6 +62,15 @@ let theatre = () => {
     }
     else{
         videoContainer.classList.add('theatre')
+    }
+}
+
+let fullScreen = () => {
+    if(videoContainer.classList.contains('fullscreen')){
+        videoContainer.classList.remove('fullscreen')
+    }
+    else{
+        videoContainer.classList.add('fullscreen')
     }
 }
 
